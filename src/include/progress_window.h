fad9724673d9b675e1bf98f179ab269586a68aba
dup <dup@localhost>
2011-10-15 10:44:41 +0000

    FSF adress and skeleton for genshahs

--- src/include/progress_window.h
+++ src/include/progress_window.h
@@ -5,7 +5,7 @@
 
    (C) Copyright 2007 - 2008 Olivier Delhomme
    e-mail : olivierdelhomme@gmail.com
- 
+
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation; either version 2, or (at your option)
@@ -18,7 +18,7 @@
 
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software Foundation,
-   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.           
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 #ifndef _PROGRESS_WINDOW_H_
 #define _PROGRESS_WINDOW_H_
